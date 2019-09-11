ed3e42522f9602b5ddc460a97d65512859cdb588
Michael Morten <mikemorten1979@gmail.com>
2019-09-11 16:44:13 -0500

    project update1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 class PagesController < ApplicationController
    def introduction
-   respond_to do |format|
+    respond_to do |format|
      format.html { render :introduction }
    end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'introduction#index'
+  get 'introduction', to: 'pages#introduction', as: 'introduction'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
